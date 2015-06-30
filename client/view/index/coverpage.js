9b6b8c12b792a57ba34387fa3854a48ba4b68159
chinhong <chinho.ng@magario.com>
2015-06-30 16:32:14 +0800

    thinking how to upload the files

--- client/view/index/coverpage.js
+++ client/view/index/coverpage.js
@@ -5,41 +5,99 @@
 var MyBrowser;
 
 Swiper = new Swipe(['page002', 'page003', 'page004', 'page005']);
+MoblieSwiper = new Swipe(['BlankTemp', 'PDFTemp']);
+
+var pdfinfo = {
+    PageMin: 1,
+    PageMax: 2,
+    PageCurrent: 1
+}
 
 Template.cover.rendered = function() {
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         MyBrowser = "Mobile";
     } else {
         MyBrowser = "Web";
     }
 
-    $('.wrapper').css({width:innerWidth, height:innerHeight});
+    $('.wrapper').css({width: innerWidth, height: innerHeight});
 
     // initial page
-    Swiper.setInitialPage('page002');
+    //Swiper.setInitialPage('page002');
 
     // page control
-    Tracker.autorun(function() {
-        if (Swiper.pageIs('page002')) {
-            Swiper.leftRight(null, 'page003');
-        }
-    });
-    Tracker.autorun(function() {
-        if (Swiper.pageIs('page003')) {
-            Swiper.leftRight('page002', 'page004');
-        }
-    });
-    Tracker.autorun(function() {
-        if (Swiper.pageIs('page004')) {
-            Swiper.leftRight('page003', 'page005');
-        }
-    });
-    Tracker.autorun(function() {
-        if (Swiper.pageIs('page005')) {
-            Swiper.leftRight('page004', null);
-        }
-    });
+    //Tracker.autorun(function() {
+    //    if (Swiper.pageIs('page002')) {
+    //        Swiper.leftRight(null, 'page003');
+    //    }
+    //});
+    //Tracker.autorun(function() {
+    //    if (Swiper.pageIs('page003')) {
+    //        Swiper.leftRight('page002', 'page004');
+    //    }
+    //});
+    //Tracker.autorun(function() {
+    //    if (Swiper.pageIs('page004')) {
+    //        Swiper.leftRight('page003', 'page005');
+    //    }
+    //});
+    //Tracker.autorun(function() {
+    //    if (Swiper.pageIs('page005')) {
+    //        Swiper.leftRight('page004', null);
+    //    }
+    //});
+
+    // Set worker URL to package assets
+    if (MyBrowser == "Mobile") {
+        $('#pdfcanvas').css({width: innerWidth, height: innerHeight});
+        PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
+        // Create PDF
+        PDFJS.getDocument("/upload/tmp/Testing.pdf").then(function getPdfHelloWorld(pdf) {
+            // Fetch the first page
+            console.log(pdf);
+            pdf.getPage(1).then(function getPageHelloWorld(page) {
+                var scale = 1;
+                var viewport = page.getViewport(scale);
+
+                // Prepare canvas using PDF page dimensions
+                var canvas = document.getElementById('pdfcanvas');
+                var context = canvas.getContext('2d');
+                canvas.height = viewport.height;
+                canvas.width = viewport.width;
+
+                // Render PDF page into canvas context
+                page.render({canvasContext: context, viewport: viewport}).promise.then(function () {
+                });
+            });
+        });
+
+        //location = "/upload/tmp/Testing.pdf";
+    }
 };
+
+function ReadPDF(p) {
+    console.log(p)
+    PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
+    // Create PDF
+    PDFJS.getDocument("/upload/tmp/Testing.pdf").then(function getPdfHelloWorld(pdf) {
+        // Fetch the first page
+        console.log(pdf);
+        pdf.getPage(p).then(function getPageHelloWorld(page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = document.getElementById('pdfcanvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            page.render({canvasContext: context, viewport: viewport}).promise.then(function () {
+            });
+        });
+    });
+}
 
 Template.cover.events({
     // Click Logon
@@ -74,26 +132,86 @@ Template.cover.events({
         }
     },
 
+    'click #PreviousPage': function() {
+        if(pdfinfo.PageCurrent > pdfinfo.PageMin) {
+            pdfinfo.PageCurrent--;
+            ReadPDF(pdfinfo.PageCurrent);
+        }
+    },
+
+    'click #NextPage': function() {
+        if(pdfinfo.PageCurrent < pdfinfo.PageMax) {
+            pdfinfo.PageCurrent++;
+            ReadPDF(pdfinfo.PageCurrent);
+        }
+    },
+
+    'click .content-item': function() {
+        location = "/upload/tmp/Testing.pdf";
+    },
+
+    //'click .content-item': function() {
+    //    console.log("!!!");
+    //    if(MyBrowser == "Mobile") {
+    //        Swiper.moveRight();
+    //        $('.list-group').hide();
+    //    }
+    //    return null;
+    //},
+
     // Controls for the wrapper
     'click .wrapper': function(e,t) {
-        if(e.screenX < innerWidth/2) {
-            Swiper.moveLeft();
-        } else {
-            Swiper.moveRight();
+        if (MyBrowser == "Web") {
+            if (e.screenX < innerWidth / 2) {
+                Swiper.moveLeft();
+            } else {
+                Swiper.moveRight();
+            }
         }
     }
 });
 
 Template.cover.helpers({
     Swiper: Swiper,
+    MoblieSwiper: MoblieSwiper,
     IsWeb: function() {
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
             return false;
         } else {
             return true;
         }
+    },
+    IsMoblie: function() {
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            return true;
+        } else {
+            return false;
+        }
     }
 });
+
+Template.PDFTemp.rendered = function() {
+    // Set worker URL to package assets
+    PDFJS.workerSrc = '/packages/pascoual_pdfjs/build/pdf.worker.js';
+    // Create PDF
+    PDFJS.getDocument("/upload/tmp/Testing.pdf").then(function getPdfHelloWorld(pdf) {
+        // Fetch the first page
+        pdf.getPage(1).then(function getPageHelloWorld(page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = document.getElementById('pdfcanvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            page.render({canvasContext: context, viewport: viewport}).promise.then(function () {
+            });
+        });
+    });
+};
 
 log = {
     active: true,
--- client/view/index/dashboard.js
+++ client/view/index/dashboard.js
@@ -2,8 +2,37 @@
  * Created by chinhong on 6/29/15.
  */
 
+Template.dashboard.created = function() {
+    //Uploader.init({
+    //    tmpDir: process.env.PWD + '/.uploads/tmp',
+    //    uploadDir: process.env.PWD + '/.uploads/',
+    //    checkCreateDirectories: true,
+    //    getDirectory: function (fileInfo, formData) {
+    //        if (formData && formData.directoryName != null) {
+    //            return formData.directoryName;
+    //        }
+    //        return "";
+    //    },
+    //    getFileName: function (fileInfo, formData) {
+    //        if (formData && formData.prefix != null) {
+    //            return formData.prefix + '_' + fileInfo.name;
+    //        }
+    //        return fileInfo.name;
+    //    },
+    //    finished: function (fileInfo, formData) {
+    //        if (formData && formData._id != null) {
+    //            Items.update({_id: formData._id}, {$push: {uploads: fileInfo}});
+    //        }
+    //    }
+    //});
+}
+
 Template.dashboard.rendered = function() {
     $('.content').show();
+
+    //Uploader.render.call(this);
+
+    Uploader.uploadUrl = Meteor.absoluteUrl("upload"); // Cordova needs absolute URL
 };
 
 Template.dashboard.events({
@@ -74,6 +103,21 @@ Template.dashboard.events({
         console.log(_headHTML);
         var _BodyHTML = $('#AddPile').find('tbody').html();
         console.log(_BodyHTML);
+    },
+
+    'click .start': function (e) {
+        console.log('!!!')
+        Uploader.startUpload.call(Template.instance(), e);
+    },
+
+    'click #SubmitAddPDF': function(e) {
+        console.log('Submiting');
+        var _directory = $('#AddPDF').find('input').eq(0).val();
+        console.log(_directory);
+        var _file = $('#AddPDF').find('input').eq(1).val();
+        console.log(_file);
+        console.log(e);
+        Uploader.startUpload.call(_file, e);
     }
 });
 
@@ -84,5 +128,32 @@ Template.dashboard.helpers({
         } else {
             return false;
         }
+    },
+    uploadCallbacks: function() {
+        return {
+            finished: function(index, fileInfo, context) {
+                console.log("Callback");
+                Uploads.insert(fileInfo);
+            }
+        }
+    },
+    'infoLabel': function() {
+        var instance = Template.instance();
+
+        // we may have not yet selected a file
+        var info = instance.info.get();
+        if (!info) {
+            return;
+        }
+
+        var progress = instance.globalInfo.get();
+
+        // we display different result when running or not
+        return progress.running ?
+        info.name + ' - ' + progress.progress + '% - [' + progress.bitrate + ']' :
+        info.name + ' - ' + info.size + 'B';
+    },
+    'progress': function() {
+        return Template.instance().globalInfo.get().progress + '%';
     }
 });
